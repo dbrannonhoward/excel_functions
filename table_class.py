f9ea8e31847ba3fb13a60c9a665fc3d4ffa8e01c
fdrotary <fdrotary@gmail.com>
2020-12-23 19:05:27 -0500

    wes experiments

---
+++ table_class.py
@@ -0,0 +1,28 @@
+class Vehicle:
+    def __init__(self, wheels=4, doors=4):
+        self.wheels=wheels
+        self.doors=doors
+        self.seats=2
+        self.steering=True
+        self.paint="montegoBlueMica"
+        self.transmission="manual"
+        self.powerSteering=False
+        self.Miata="AlwaysTheAnswer"
+class Lotus(Vehicle):
+    def __init__(self):
+        super().__init__()
+        self.weight="light"
+    def weigh_car(self, weight: int):
+        if weight < 2000:
+            self.weight="Light"
+            return
+        self.weight="Not Light"
+
+
+myCar=Lotus()
+branCar=Lotus()
+myCar.weigh_car(weight=2)
+branCar.weigh_car(weight=22222222222222222222222)
+
+
+pass
