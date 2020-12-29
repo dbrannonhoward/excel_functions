f5af28197456297cee8231449a67125ed1d31dc2
fdrotary <fdrotary@gmail.com>
2020-12-28 19:19:54 -0500

    random stuff

--- excel_test_code.py
+++ excel_test_code.py
@@ -9,7 +9,7 @@ class WesTest:
             print('Creation SUCCESS')
         else:
             print('you FAILED')
-    def cell_array (self, num_of_row=1, num_of_col=100):
+    def open_existing_excel_workbook_creates_array_of_cell_values (self, num_of_row=1, num_of_col=100):
         excel = ExcelFile()
         excel.open_existing_workbook(my_name_for_my_excel_file)
         for i in range(1, num_of_col+1):
@@ -20,6 +20,14 @@ class WesTest:
                 print(col + row)
                 excel.write_cell_value(col, row, col+row)
         excel.save_changes()
+    def copy_excel_cell_from_one_sheet_to_another(self, sheet_one):
+        col = 'A'
+        row = '1'
+        excel = ExcelFile()
+        excel.open_existing_workbook(sheet_one)
+        print(excel.read_cell_value(col, row))
+
+
 
 
 
@@ -29,8 +37,11 @@ if __name__ == '__main__':
     my_name_for_my_excel_file = 'tomato'
     cell_entry = 'cell entry I made'
     wt = WesTest()
-    wt.create_excel_workbook(my_name_for_my_excel_file)
-    wt.cell_array(100, 10)
-    excel = ExcelFile()
-    excel.open_existing_workbook(my_name_for_my_excel_file)
-    #excel.write_cell_value(col, row, cell_entry)
+    #wt.create_excel_workbook(my_name_for_my_excel_file)
+    #wt.open_existing_excel_workbook_creates_array_of_cell_values(1000, 26)
+    wt.copy_excel_cell_from_one_sheet_to_another(my_name_for_my_excel_file)
+
+
+
+    #excel = ExcelFile()
+    #excel.open_existing_workbook(my_name_for_my_excel_file)
